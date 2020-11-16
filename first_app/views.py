6bf6ca2da3dbf0b412092c398ab55f0b80451b0e
herlianzhang <herlianzhang370@gmail.com>
2020-11-16 22:01:52 +0700

    first commit

---
+++ first_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def index(request):
+    my_dict = {'insert_content':"HELLO IM FROM FIRSTAPP!"}
+    return render(request, 'first_app/index.html', context=my_dict)
+
